9de76c03a0c3e80b0b2d98de3193454dbf8569b4
Particle <particlecore@outlook.com>
2015-10-12 13:30:03 +0000

    Update

--- Javascript/mainscript.js
+++ Javascript/mainscript.js
@@ -1,10 +1,15 @@
 function toggleMenu() {
+
     document.getElementById("main_page").classList.toggle("show");
+
 }
 
 if (window.navigator.userAgent.indexOf("Android") > -1) {
+
     document.documentElement.setAttribute("operatingsystem", "Android");
+
 } else if (window.navigator.userAgent.indexOf("iP") > -1) {
+
     document.documentElement.setAttribute("operatingsystem", "iOS");
+
 }
-window.alert(window.navigator.userAgent.indexOf("Android") > -1);
