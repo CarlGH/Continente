8945d2604e99e2773977ab428739270f881988dc
Particle <particlecore@outlook.com>
2015-10-14 19:52:30 +0000

    Update

--- Javascript/mainscript.js
+++ Javascript/mainscript.js
@@ -1,5 +1,27 @@
+var paginaActual = document.getElementById("ativar_" + window.location.hash);
+
 function toggleMenu() {
 
     document.body.classList.toggle("show");
+
+}
+
+function navigateMenu(event) {
+
+    window.location.hash = event.target.className;
+    document.getElementById("page_title").textContent = event.target.textContent;
+    toggleMenu();
+
+    console.log(event);
+
+}
+
+if (paginaActual) {
+
+    paginaActual.checked = true;
+
+} else {
+
+    document.getElementById("ativar_inicio").checked = true;
 
 }
