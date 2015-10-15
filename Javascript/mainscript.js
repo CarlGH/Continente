3d485b59d282fa86e9a2a9d2303dc4f8b200e798
Particle <particlecore@outlook.com>
2015-10-15 15:55:25 +0000

    Update

--- Javascript/mainscript.js
+++ Javascript/mainscript.js
@@ -1,3 +1,39 @@
+function xhr(method, url, callback) {
+    var request = new XMLHttpRequest;
+
+    request.addEventListener("load", callback);
+    //request.open("GET", "http://m.folhetos.continente.pt/scripts/get_stores.php", true);
+    request.open("GET", url, true);
+    request.send();
+
+}
+
+function openMap(event) {
+    /*var googleApi = document.createElement("script");
+    var googleJsApi = document.createElement("script");
+    var mapaLojas = document.createElement("script");
+
+    googleApi.setAttribute("type", "text/javascript");
+    googleApi.src = "http://maps.googleapis.com/maps/api/js?sensor=false";
+    googleJsApi.setAttribute("type", "text/javascript");
+    googleJsApi.src = "http://www.google.com/jsapi";
+    mapaLojas.setAttribute("type", "text/javascript");
+    mapaLojas.src = "Javascript/mapalojas.js";
+
+    event.parentNode.appendChild(googleApi);
+    event.parentNode.appendChild(googleJsApi);
+    event.parentNode.appendChild(mapaLojas);*/
+
+    //document.getElementById("mapa_lojas").src = "http://m.folhetos.continente.pt/lojas_fenix.html";
+    var mapa = document.getElementById("mapa_lojas");
+    if (!mapa.src) {
+        mapa.src = "Paginas/googlemap.html";
+        console.log(event);
+    }
+    document.getElementById("mapa").classList.toggle("show");
+
+}
+
 function toggleMenu() {
 
     document.body.classList.toggle("show");
@@ -15,7 +51,7 @@ function navigateMenu(event) {
     document.getElementById("page_title").textContent = event.target.textContent;
 
     closeMenu();
-console.log(event);
+    console.log(event);
 }
 
 function navigateHash() {
@@ -25,9 +61,11 @@ function navigateHash() {
     if (paginaAtual) {
 
         paginaAtual.checked = true;
-        
+
         if (menuAtual) {
+
             document.getElementById("page_title").textContent = document.querySelector("[for='ativar_" + window.location.hash.substring(1) + "']").textContent;
+        
         }
 
     } else {
